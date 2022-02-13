d313d9ff7d6432a017a92bbd6c78cc5aa75dff1e
Gavrilova Maria <82565971+MaryGavrilova@users.noreply.github.com>
2022-02-13 14:12:24 +0000

    Delete InvalidCredentialsException.java

--- src/main/java/ru/netology/cloudstorage/exception/InvalidCredentialsException.java
+++
@@ -1,8 +0,0 @@
-package ru.netology.cloudstorage.exception;
-
-public class InvalidCredentialsException extends RuntimeException {
-
-    public InvalidCredentialsException(String msg) {
-        super(msg);
-    }
-}
