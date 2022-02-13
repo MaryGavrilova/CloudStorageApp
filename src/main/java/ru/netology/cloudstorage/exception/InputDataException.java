08df75bbef70847da01f1a0f2d32f85109288e20
Gavrilova Maria <82565971+MaryGavrilova@users.noreply.github.com>
2022-02-13 14:12:17 +0000

    Delete InputDataException.java

--- src/main/java/ru/netology/cloudstorage/exception/InputDataException.java
+++
@@ -1,8 +0,0 @@
-package ru.netology.cloudstorage.exception;
-
-public class InputDataException extends RuntimeException {
-
-    public InputDataException(String msg) {
-        super(msg);
-    }
-}
