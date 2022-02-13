4b1b5861f4a3b58e558fc1feb40cbcea1541a589
Gavrilova Maria <82565971+MaryGavrilova@users.noreply.github.com>
2022-02-13 14:12:36 +0000

    Delete UnauthorizedException.java

--- src/main/java/ru/netology/cloudstorage/exception/UnauthorizedException.java
+++
@@ -1,8 +0,0 @@
-package ru.netology.cloudstorage.exception;
-
-public class UnauthorizedException extends RuntimeException {
-
-    public UnauthorizedException(String msg) {
-        super(msg);
-    }
-}
